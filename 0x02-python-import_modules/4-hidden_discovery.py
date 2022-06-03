632f4e25d73691f6954dcd5d9ac4337f8b69add0
ekan07 <aniekanbon@gmail.com>
2022-06-03 02:08:11 +0100

    Add files: 4-hidden_discovery.py, 5-variable_load.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def discover(hidden):
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    import hidden_4
+    discover(hidden_4)
