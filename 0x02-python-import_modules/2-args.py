fe336c8c56c7a45c8ff14e0b5345a0d4c3fb0506
ekan07 <aniekanbon@gmail.com>
2022-06-03 01:10:12 +0100

    Modify file: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 def print_arg(argv):
     arg_len = len(argv) - 1
-    if arg_len == 0:
-        print("{:d} argument.".format(arg_len))
-        return
+    if arg_len == 1:
+        print("{:d} argument:".format(arg_len))
     else:
-        if arg_len == 1:
-            print("{:d} argument:".format(arg_len))
-        else:
-            print("{:d} arguments:".format(arg_len))
-        i = 1
-        while i <= arg_len:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+        print("{:d} arguments:".format(arg_len))
+    i = 1
+    while i <= arg_len:
+        print("{:d}: {:s}".format(i, argv[i]))
+        i += 1
 
 
 if __name__ == "__main__":
