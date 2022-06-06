86f0bd568567f1eddcb63e6cd4715ab5779a5f2b
ekan07 <aniekanbon@gmail.com>
2022-06-06 17:22:45 +0100

    Add files: 4-new_in_list.py, 5-no_c.py, 5-print_matrix_integer.py, 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    for i in range(2):
+        if len(tuple_a) < 2:
+            tuple_a += 0,
+        if len(tuple_b) < 2:
+            tuple_b += 0,
+        new_tuple += tuple_a[i] + tuple_b[i],
+    return new_tuple
