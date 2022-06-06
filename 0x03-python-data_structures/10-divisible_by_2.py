6cab5d27a54d663116b3ab7e73064f4e97cf1887
ekan07 <aniekanbon@gmail.com>
2022-06-07 00:49:55 +0100

    Modify file: 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def divisible_by_2(my_list=[]):
     l_itm = []
-    for i in range(len(my_list)):
-        itm = True if my_list[i] % 2 == 0 else False
+    for count, _ in enumerate(my_list):
+        itm = True if my_list[count] % 2 == 0 else False
         l_itm.append(itm)
     return l_itm
