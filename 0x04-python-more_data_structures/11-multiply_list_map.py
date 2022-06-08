d50ee5fd9d653262e7947dd90e3522412730d393
ekan07 <aniekanbon@gmail.com>
2022-06-08 21:38:25 +0100

    Add file: multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,8 +2,3 @@
 def multiply_list_map(my_list=[], number=0):
     res = list(map(lambda a: number * a, my_list))
     return res
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
