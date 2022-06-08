36a35f0c8272c2e439ec8b93faa2f341b6bb97e7
ekan07 <aniekanbon@gmail.com>
2022-06-08 21:12:26 +0100

    Modify file: 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,12 +3,3 @@ def multiply_by_2(a_dictionary):
     res = a_dictionary.copy()
     res = {k: 2 *res[k] for k in res}
     return res
-
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
