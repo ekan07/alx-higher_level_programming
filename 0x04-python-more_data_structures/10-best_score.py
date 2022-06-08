b63447aa654880fd4a65965a5570c9d6cd4c0503
ekan07 <aniekanbon@gmail.com>
2022-06-09 00:29:23 +0100

    Modify files: 10-best_score.py, 11-multiply_list_map.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    s = sorted(a_dictionary, key=lambda a: a_dictionary.get(a), reverse=True)
+    return s[0]
+    
