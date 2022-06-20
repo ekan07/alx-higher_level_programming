8675d041049410600fdd8ea43789b04eb5bf030e
ekan07 <aniekanbon@gmail.com>
2022-06-20 23:18:05 +0100

    Modify files: 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,22 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        if int(value):
+            return True
     except ValueError:
         return False
-    else:
-        return True
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
