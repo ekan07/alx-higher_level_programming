babf964a3cf207b15bf9c953c857b5e544578c4d
ekan07 <aniekanbon@gmail.com>
2022-07-01 11:09:37 +0100

    Modify files: 100-matrix_mul.py, 101-lazy_matrix_mul.py, 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,12 +6,13 @@ Adds two integer together
 
 
 def add_integer(a, b=98):
-    """returns sum of a & b
-    Args:
-        a: int
-        b: int
     """
-
+    Adds two integer.
+    Args:
+        a (int|float): An integer or floating point number
+        b (int|float): An integer or floating point number
+    Returns: The sum of `a` and `b`
+    """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     elif not isinstance(b, (int, float)):
