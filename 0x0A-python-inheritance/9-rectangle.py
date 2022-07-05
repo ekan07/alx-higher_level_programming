368d7824cbb967a1145a263b45f3d74f8ace88ed
ekan07 <aniekanbon@gmail.com>
2022-07-05 04:37:18 +0100

    Modify files: 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         """Computer area of rectangle."""
         return (self.__width * self.__height)
