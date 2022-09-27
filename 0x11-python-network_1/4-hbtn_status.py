788b1d8e7bb5b27b6cc7237415d7863885befe27
ekan07 <aniekanbon@gmail.com>
2022-09-27 02:24:01 +0100

    Update file: 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 
     req = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
+    print(' '*4+'- type: {}'.format(type(req.text)))
+    print(' '*4+'- content: {}'.format(req.text))
